10a8c175cb254357b2b9b3300a4e5eee66f11280
Leonaldi Nata Gunawan <leonaldi.gunawan@xtremax.com>
2023-08-21 15:20:06 +0700

    first commit

---
+++ EntityFrameworkTutorial1/EntityFrameworkTutorial1/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using EntityFrameworkTutorial1.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkTutorial1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=M21-010B;Database=EFTutorialYoutube;User Id=xtremax_sa;Password=th3ZetaLion!;" +
+                "Trusted_Connection=false;TrustServerCertificate=true;MultipleActiveResultSets=true;");
+        }
+    }
+}
---
+++ EntityFrameworkTutorial1/EntityFrameworkTutorial1/Model/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkTutorial1.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
